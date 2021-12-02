611cad56a02759c1ea33268bf6683c7d06a35ca9
Deleplace <deleplace2015@gmail.com>
2021-12-02 16:45:08 +0100

    VDE v3.1, 43 ns/op.

--- day-1-1/main.go
+++ day-1-1/main.go
@@ -25,45 +25,39 @@ func run() error {
 }
 
 func Answer(input string) (int, error) {
-	var prev struct {
-		val int64
-		set bool
+	var prev int16 = -9999
+	var increases int = -1
+
+	// Read 1st char of 1st number
+	c := input[0]
+	val := int16(c - '0')
+mainloop:
+	for p, N := 1, len(input); p < N; p++ {
+		c := input[p]
+		//fmt.Printf("input[%d] is %c (%d)\n", p, c, c)
+		switch {
+		case c == '\n':
+			if val > prev {
+				//fmt.Println("Found", val, ">", prev, " !!!!!")
+				increases++
+			}
+			//fmt.Println(prev, val, increases, " p =", p)
+			prev = val
+			// Read 1st char of next number
+			p++
+			if p >= N {
+				break mainloop
+			}
+			c := input[p]
+			val = int16(c - '0')
+		case c >= '0' && c <= '9':
+			val = val*10 + int16(c-'0')
+		default:
+			panic("A")
+		}
 	}
-	var increases int
-	var line string
-	for len(input) > 0 {
-		i := strings.IndexByte(input, '\n')
-		if i == -1 {
-			line = input
-			input = ""
-		} else {
-			line = input[0:i]
-			input = input[i+1:]
-		}
-		val, err := parseInt(line)
-		if err != nil {
-			return 0, err
-		}
-		if val > prev.val && prev.set {
-			increases++
-		}
-		prev.set = true
-		prev.val = val
+	if val > prev {
+		increases++
 	}
 	return increases, nil
-}
-
-func parseInt(val string) (int64, error) {
-	var intVal int64
-	factor := int64(1)
-	for i := len(val) - 1; i >= 0; i-- {
-		c := val[i]
-		if c >= '0' && c <= '9' {
-			intVal += int64(c-'0') * factor
-		} else {
-			return intVal, fmt.Errorf("bad int: %q", val)
-		}
-		factor *= 10
-	}
-	return intVal, nil
 }
